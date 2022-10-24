0d2516c04a6983e2926da3d0f8f9fbd39289d8ad
cristian <espaacristian@gmail.com>
2022-10-23 22:04:33 -0500

    corrección-imágenes

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 
     export const getGifs = async(category)=>{
 
-        const url= `http://api.giphy.com/v1/gifs/search?api_key=UddxIXf7XVLwThGrP9MPMV70ELmhAig4&q=${category}&limit=10`;
+        const url= `https://api.giphy.com/v1/gifs/search?api_key=UddxIXf7XVLwThGrP9MPMV70ELmhAig4&q=${category}&limit=10`;
         const resp= await fetch(url);
         const {data} = await resp.json();
         console.log(resp);
